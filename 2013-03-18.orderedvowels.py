1ec8c11f169a49e3bb28f44b3619ced10c43eaab
Freddie Sanchez <freddie.sanchez@gmail.com>
2013-03-18 09:17:59 -0700

    comments added

--- 2013-03-18.orderedvowels.py
+++ 2013-03-18.orderedvowels.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python
+
+"""
+http://www.reddit.com/r/dailyprogrammer/comments/1aih0v/031813_challenge_122_easy_words_with_ordered/
+
+Find words in a word list that contain all the vowels in alphabetical order, 
+non-repeated, where vowels are defined as A E I O U Y.
+"""
+
 import re, sys
 
 def main():
